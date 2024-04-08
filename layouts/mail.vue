dc0a1e035569a87df57065611ba46506b78c0c28
ykjung <ykjung@ecomarinesys.co.kr>
2024-04-09 08:58:54 +0900

    선택박스 수정

--- layouts/mail.vue
+++ layouts/mail.vue
@@ -6,7 +6,22 @@ const navCollapsedSize = 4;
   <TooltipProvider :delay-duration="0">
     <ResizablePanelGroup id="resize-panel-group-1" direction="horizontal" class="h-full items-stretch">
       <ResizablePanel id="resize-panel-1" :default-size="defaultLayout[0]" :collapsed-size="navCollapsedSize" collapsible :min-size="15" :max-size="20" class="h-screen flex flex-col">
-        <div class="px-4 py-3.5 font-bold text-xl shrink-0">Inpiniti</div>
+        <div class="pl-4 p-2 shrink-0">
+          <div class="flex items-center justify-between">
+            <div class="font-bold text-xl">Inpiniti</div>
+            <Select>
+              <SelectTrigger class="w-fit">
+                <SelectValue placeholder="select" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Fruits</SelectLabel>
+                  <SelectItem value="apple"> Apple </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
         <Separator />
         <LayoutsNavBooks />
         <!-- <Nav :is-collapsed="isCollapsed" :links="links" /> -->
@@ -17,8 +32,10 @@ const navCollapsedSize = 4;
           <div class="flex items-center px-4 py-2">
             <h1 class="text-xl font-bold">Inbox</h1>
             <TabsList class="ml-auto">
-              <TabsTrigger value="all" class="text-zinc-600 dark:text-zinc-200"> All mail </TabsTrigger>
-              <TabsTrigger value="unread" class="text-zinc-600 dark:text-zinc-200"> Unread </TabsTrigger>
+              <TabsTrigger value="all" class="text-zinc-600 dark:text-zinc-200"> 도서 </TabsTrigger>
+              <TabsTrigger value="unread" class="text-zinc-600 dark:text-zinc-200"> 음악 </TabsTrigger>
+              <TabsTrigger value="unread" class="text-zinc-600 dark:text-zinc-200"> 게시판 </TabsTrigger>
+              <TabsTrigger value="unread" class="text-zinc-600 dark:text-zinc-200"> 피규어 </TabsTrigger>
             </TabsList>
           </div>
           <Separator />
