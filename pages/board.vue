881ebfa4333618cce2550bbc2398e4bbcfc30f49
youngkyunjung <younginpiniti@gmail.com>
2024-04-22 07:34:34 +0900

    글작성 페이지 작업중

--- pages/board.vue
+++ pages/board.vue
@@ -7,7 +7,11 @@ const verifiedEmails = ref(["m@example.com", "m@google.com", "m@support.com"]);
 const onSubmit = (values) => {
   toast({
     title: "You submitted the following values:",
-    description: h("pre", { class: "mt-2 w-[340px] rounded-md bg-slate-950 p-4" }, h("code", { class: "text-white" }, JSON.stringify(values, null, 2))),
+    description: h(
+      "pre",
+      { class: "mt-2 w-[340px] rounded-md bg-slate-950 p-4" },
+      h("code", { class: "text-white" }, JSON.stringify(values, null, 2))
+    ),
   });
 };
 </script>
@@ -15,19 +19,39 @@ const onSubmit = (values) => {
   <div class="p-4 flex flex-col gap-6">
     <div>
       <h3 class="text-lg font-medium">게시판 글작성</h3>
-      <p class="text-sm text-muted-foreground">주술회전 게시판 입니다. 다른 게시판에 글을 작성하고자 한다면 다른 게시판을 선택해 주세요.</p>
+      <p class="text-sm text-muted-foreground">
+        주술회전 게시판 입니다. 다른 게시판에 글을 작성하고자 한다면 다른
+        게시판을 선택해 주세요.
+      </p>
     </div>
     <Separator />
     <div class="flex flex-col gap-2">
-      <h5 class="text-sm font-semibold text-neutral-600">Username</h5>
-      <Input placeholder="shadcn"></Input>
-      <p class="text-sm text-muted-foreground">This is your public display name. It can be your real name or a pseudonym. You can only change this once every 30 days.</p>
+      <h5 class="text-sm font-semibold text-neutral-600">제목</h5>
+      <Input placeholder="제목을 입력해주세요."></Input>
     </div>
     <div class="flex flex-col gap-2">
-      <h5 class="text-sm font-semibold text-neutral-600">Email</h5>
-      <Input placeholder="Select an email"></Input>
-      <p class="text-sm text-muted-foreground">You can manage verified email addresses in your email settings.</p>
+      <h5 class="text-sm font-semibold text-neutral-600">작성자</h5>
+      <Input placeholder="작성자를 작성해주세요."></Input>
     </div>
-    <div class="flex gap-2"><Button size="sm">Update profile</Button><Button size="sm" variant="outline">Cancel</Button></div>
+    <div class="flex flex-col gap-2">
+      <h5 class="text-sm font-semibold text-neutral-600">도서</h5>
+      <Input placeholder="도서를 선택해 주세요."></Input>
+    </div>
+    <div class="flex flex-col gap-2">
+      <h5 class="text-sm font-semibold text-neutral-600">말머리</h5>
+      <Input placeholder="말머리를 선택해 주세요."></Input>
+    </div>
+    <div class="flex flex-col gap-2">
+      <h5 class="text-sm font-semibold text-neutral-600">내용</h5>
+      <Input placeholder="내용을 입력해주세요."></Input>
+    </div>
+    <div class="flex flex-col gap-2">
+      <h5 class="text-sm font-semibold text-neutral-600">Image</h5>
+      <Input placeholder="Select an email"></Input>
+    </div>
+    <div class="flex gap-2">
+      <Button size="sm">등록</Button
+      ><Button size="sm" variant="outline">취소</Button>
+    </div>
   </div>
 </template>
